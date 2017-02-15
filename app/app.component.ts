8a8ee9e99ac8b53d4ccb2937194ade142759eafe
kylehu16 <deanhh9@gmail.com>
2017-02-16 00:12:03 +0800

    新增

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector:'my-app',
+    template:`
+        <h1>{{title}}</h1>
+        <nav>
+            <a routerLink="/dashboard" routerLink="active">Dashboard</a>
+            <a routerLink="/heroes" routerLink="active">Heroes</a>
+        </nav>
+        <router-outlet></router-outlet>
+    `,
+    styleUrls:['/app.component.css']
+})
+
+export class AppComponent{
+    title='Tour of Heroes';
+}
