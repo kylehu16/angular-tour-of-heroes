4c86f0389170238c8c9dd35c77d75064a804726f
kylehu16 <deanhh9@gmail.com>
2017-02-16 17:52:01 +0800

    修改

--- app/hero.service.js
+++ app/hero.service.js
@@ -15,7 +15,12 @@ var HeroService = (function () {
     function HeroService(http) {
         this.http = http;
         this.heroesUrl = 'app/heroes'; // URL to web api
+        this.headers = new http_1.Headers({ 'Content-Type': 'application/json' });
     }
+    HeroService.prototype.handleError = function (error) {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    };
     HeroService.prototype.getHeroes = function () {
         return this.http.get(this.heroesUrl)
             .toPromise()
@@ -33,9 +38,27 @@ var HeroService = (function () {
         return this.getHeroes()
             .then(function (heroes) { return heroes.find(function (hero) { return hero.id === id; }); });
     };
-    HeroService.prototype.handleError = function (error) {
-        console.error('An error occurred', error); // for demo purposes only
-        return Promise.reject(error.message || error);
+    HeroService.prototype.update = function (hero) {
+        var url = this.heroesUrl + "/" + hero.id;
+        return this.http
+            .put(url, JSON.stringify(hero), { headers: this.headers })
+            .toPromise()
+            .then(function () { return hero; })
+            .catch(this.handleError);
+    };
+    HeroService.prototype.create = function (name) {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
+            .toPromise()
+            .then(function (res) { return res.json().data; })
+            .catch(this.handleError);
+    };
+    HeroService.prototype.delete = function (id) {
+        var url = this.heroesUrl + "/" + id;
+        return this.http.delete(url, { headers: this.headers })
+            .toPromise()
+            .then(function () { return null; })
+            .catch(this.handleError);
     };
     HeroService = __decorate([
         core_1.Injectable(), 
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -9,9 +9,13 @@ import 'rxjs/add/operator/toPromise';
 export class HeroService{
 
     private heroesUrl = 'app/heroes';  // URL to web api
-
+    private headers=new Headers({'Content-Type':'application/json'});
     constructor(private http: Http) { }
 
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }    
     getHeroes(): Promise<Hero[]> {
     return this.http.get(this.heroesUrl)
                .toPromise()
@@ -28,10 +32,29 @@ export class HeroService{
             .then(heroes=>heroes.find(hero=>hero.id===id));
     }
 
-    private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
-        return Promise.reject(error.message || error);
+    update(hero:Hero):Promise<Hero>{
+        const url=`${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url,JSON.stringify(hero),{headers:this.headers})
+            .toPromise()
+            .then(()=>hero)
+            .catch(this.handleError);
     }
+    create(name:string):Promise<Hero>{
+        return this.http
+            .post(this.heroesUrl,JSON.stringify({name:name}),{headers:this.headers})
+            .toPromise()
+            .then(res=>res.json().data)
+            .catch(this.handleError);
+    }
+    delete(id:number):Promise<void>{
+        const url=`${this.heroesUrl}/${id}`;
+        return this.http.delete(url,{headers:this.headers})
+        .toPromise()
+        .then(()=>null)
+        .catch(this.handleError);
+    }
+
     
 }
 
